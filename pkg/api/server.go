fd072cce474ec78c2bcc3422de8ef22b99dda80b
Mikhail Kalinin <m.kalinin@selectel.ru>
2018-07-20 00:38:04 +0300

    Add json content-type

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 )
 
 func StartAPIServer(ip string, port int) error {
@@ -16,6 +17,7 @@ func StartAPIServer(ip string, port int) error {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
 
 	r.Route("/sales", func(r chi.Router) {
 		r.Get("/", listSales)
